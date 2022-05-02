b2c5ce7ac8aec1c1303fcc6ff6c57c7720769e16
Omer Herrera <omer.h@bizzabo.com>
2022-05-02 10:44:38 +0300

    - Add route to remote app in reat-host-remote type

--- react-host-remote/remote/src/App.jsx
+++ react-host-remote/remote/src/App.jsx
@@ -1,11 +1,21 @@
 import React from 'react';
+import { Link } from "react-router-dom";
 
 function App() {
   return (
     <>
       <div>
-        This is the Remote App
+        This is the Remote App ;-) 
       </div>
+      <nav
+        style={{
+          borderBottom: "solid 1px",
+          paddingBottom: "1rem"
+        }}
+      >
+        <Link to="/invoices">Invoices</Link> |{" "}
+        <Link to="/expenses">Expenses</Link>
+      </nav>
     </>
       
   );
